fbc4d41d337af06695cb980344d3ae4cff244091
Rodolfo Santos <rodolfodossantos29@gmail.com>
2022-10-21 17:00:23 -0300

    🚀 update server config to try deploy at heroku

--- nuxt.config.js
+++ nuxt.config.js
@@ -72,7 +72,7 @@ export default {
 
   buildModules: ['@nuxt/typescript-build', '@nuxtjs/vuetify', '@nuxtjs/style-resources', '@nuxt/image'],
 
-  build: {},
+  server: { port: process.env.PORT || 3000 },
 
-  server: { port: process.env.PORT || 3000 }
+  build: {}
 };
